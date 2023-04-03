59b40ff2600af1845784e7641f80ffda74d0db85
Hussein <eng.anabtawi@gmail.com>
2023-04-04 00:18:33 +0300

    Remove firebase Config

--- src/Services/firebase.js
+++ src/Services/firebase.js
@@ -3,13 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCDMzlC1btBg2nFJZKZdwIIGS-hWIupyaE",
-    authDomain: "clone-4c5a6.firebaseapp.com",
-    projectId: "clone-4c5a6",
-    storageBucket: "clone-4c5a6.appspot.com",
-    messagingSenderId: "770890504457",
-    appId: "1:770890504457:web:73d69d4ce819cf90e15bd8",
-    measurementId: "G-VL02N9447R"
+    
 };
 
 // Initialize Firebase
