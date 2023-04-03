164bd65b43e4e61490f4d62521cbf64d985464e1
David <dydyd289@gmail.com>
2023-04-03 14:54:50 +0300

    update my work

--- src/Components/VideoCard/VideoCard.tsx
+++ src/Components/VideoCard/VideoCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Avatar, IconButton } from '@mui/material'
 import { ModalVideo } from './ModalVideo';
 import ClosedCaptionOutlinedIcon from '@mui/icons-material/ClosedCaptionOutlined';
@@ -7,17 +7,17 @@ import VolumeOffOutlinedIcon from '@mui/icons-material/VolumeOffOutlined';
 import ClosedCaptionDisabledOutlinedIcon from '@mui/icons-material/ClosedCaptionDisabledOutlined';
 import './videoCard.css'
 
-export const VideoCard = () => {
+export const VideoCard = (): JSX.Element => {
 
 const [volume, setVolume] = React.useState(true)
 const [subtitle, setSubtitle] = React.useState(true)
 
 
-const handleVolume = () => {
+const handleVolume = (): void => {
     setVolume(prev => !prev)
 }
 
-const handleSubtitle = () => {
+const handleSubtitle = (): void => {
     setSubtitle(prev => !prev)
 }
 
@@ -27,7 +27,7 @@ const handleSubtitle = () => {
 
   <div className='videoCardDiv'>
     <div className='image'>
-        <img className='imageTeaser' src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"  />
+        <img className='imageTeaser' src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt='Learen JavaScript' />
     </div>
         <div className='timeVideoDiv'><span className='timeVideo'>20:45</span></div>
 
@@ -65,6 +65,215 @@ const handleSubtitle = () => {
             <p className='channelName'><span>2 days ago</span> <span className='dot'>•</span>views 3,847 </p>
     </div>
   </div>
+
+  <div className='videoCardDiv'>
+    <div className='image'>
+        <img className='imageTeaser' src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt='Learen JavaScript' />
+    </div>
+        <div className='timeVideoDiv'><span className='timeVideo'>20:45</span></div>
+
+        <div className='hide'><span className='volumeAndSubtitle'>
+        <IconButton onClick={handleVolume}>
+        {volume ? <VolumeUpOutlinedIcon sx={{color: 'white'}}/> :
+         <VolumeOffOutlinedIcon sx={{color: 'white'}}/>}
+        </IconButton>
+         | 
+         <IconButton onClick={handleSubtitle}>
+         {subtitle ? <ClosedCaptionOutlinedIcon sx={{color: 'white'} }/> :
+         <ClosedCaptionDisabledOutlinedIcon sx={{color: 'white'} }/>}
+         </IconButton>
+            </span></div>
+
+    <div className='headerAndAvatatr'>
+        <div className='avatar'>
+            <IconButton>
+                <Avatar src='https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg' />
+            </IconButton>
+        </div>
+        <div className='header'>
+            <p className='headerText'>test your skills in Javascript in our toturial</p>
+        </div>
+        <div className='moreOption'>
+            
+            <ModalVideo/>
+            
+        </div>
+    </div>  
+    <div className='channelNameDiv'>
+            <p className='channelName'>Javascript</p>
+    </div>
+    <div className='whatching'>
+            <p className='channelName'><span>2 days ago</span> <span className='dot'>•</span>views 3,847 </p>
+    </div>
+  </div>
+
+
+  <div className='videoCardDiv'>
+    <div className='image'>
+        <img className='imageTeaser' src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt='Learen JavaScript' />
+    </div>
+        <div className='timeVideoDiv'><span className='timeVideo'>20:45</span></div>
+
+        <div className='hide'><span className='volumeAndSubtitle'>
+        <IconButton onClick={handleVolume}>
+        {volume ? <VolumeUpOutlinedIcon sx={{color: 'white'}}/> :
+         <VolumeOffOutlinedIcon sx={{color: 'white'}}/>}
+        </IconButton>
+         | 
+         <IconButton onClick={handleSubtitle}>
+         {subtitle ? <ClosedCaptionOutlinedIcon sx={{color: 'white'} }/> :
+         <ClosedCaptionDisabledOutlinedIcon sx={{color: 'white'} }/>}
+         </IconButton>
+            </span></div>
+
+    <div className='headerAndAvatatr'>
+        <div className='avatar'>
+            <IconButton>
+                <Avatar src='https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg' />
+            </IconButton>
+        </div>
+        <div className='header'>
+            <p className='headerText'>test your skills in Javascript in our toturial</p>
+        </div>
+        <div className='moreOption'>
+            
+            <ModalVideo/>
+            
+        </div>
+    </div>  
+    <div className='channelNameDiv'>
+            <p className='channelName'>Javascript</p>
+    </div>
+    <div className='whatching'>
+            <p className='channelName'><span>2 days ago</span> <span className='dot'>•</span>views 3,847 </p>
+    </div>
+  </div>
+
+
+  <div className='videoCardDiv'>
+    <div className='image'>
+        <img className='imageTeaser' src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt='Learen JavaScript' />
+    </div>
+        <div className='timeVideoDiv'><span className='timeVideo'>20:45</span></div>
+
+        <div className='hide'><span className='volumeAndSubtitle'>
+        <IconButton onClick={handleVolume}>
+        {volume ? <VolumeUpOutlinedIcon sx={{color: 'white'}}/> :
+         <VolumeOffOutlinedIcon sx={{color: 'white'}}/>}
+        </IconButton>
+         | 
+         <IconButton onClick={handleSubtitle}>
+         {subtitle ? <ClosedCaptionOutlinedIcon sx={{color: 'white'} }/> :
+         <ClosedCaptionDisabledOutlinedIcon sx={{color: 'white'} }/>}
+         </IconButton>
+            </span></div>
+
+    <div className='headerAndAvatatr'>
+        <div className='avatar'>
+            <IconButton>
+                <Avatar src='https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg' />
+            </IconButton>
+        </div>
+        <div className='header'>
+            <p className='headerText'>test your skills in Javascript in our toturial</p>
+        </div>
+        <div className='moreOption'>
+            
+            <ModalVideo/>
+            
+        </div>
+    </div>  
+    <div className='channelNameDiv'>
+            <p className='channelName'>Javascript</p>
+    </div>
+    <div className='whatching'>
+            <p className='channelName'><span>2 days ago</span> <span className='dot'>•</span>views 3,847 </p>
+    </div>
+  </div>
+
+  <div className='videoCardDiv'>
+    <div className='image'>
+        <img className='imageTeaser' src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt='Learen JavaScript' />
+    </div>
+        <div className='timeVideoDiv'><span className='timeVideo'>20:45</span></div>
+
+        <div className='hide'><span className='volumeAndSubtitle'>
+        <IconButton onClick={handleVolume}>
+        {volume ? <VolumeUpOutlinedIcon sx={{color: 'white'}}/> :
+         <VolumeOffOutlinedIcon sx={{color: 'white'}}/>}
+        </IconButton>
+         | 
+         <IconButton onClick={handleSubtitle}>
+         {subtitle ? <ClosedCaptionOutlinedIcon sx={{color: 'white'} }/> :
+         <ClosedCaptionDisabledOutlinedIcon sx={{color: 'white'} }/>}
+         </IconButton>
+            </span></div>
+
+    <div className='headerAndAvatatr'>
+        <div className='avatar'>
+            <IconButton>
+                <Avatar src='https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg' />
+            </IconButton>
+        </div>
+        <div className='header'>
+            <p className='headerText'>test your skills in Javascript in our toturial</p>
+        </div>
+        <div className='moreOption'>
+            
+            <ModalVideo/>
+            
+        </div>
+    </div>  
+    <div className='channelNameDiv'>
+            <p className='channelName'>Javascript</p>
+    </div>
+    <div className='whatching'>
+            <p className='channelName'><span>2 days ago</span> <span className='dot'>•</span>views 3,847 </p>
+    </div>
+  </div>
+
+
+  <div className='videoCardDiv'>
+    <div className='image'>
+        <img className='imageTeaser' src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt='Learen JavaScript' />
+    </div>
+        <div className='timeVideoDiv'><span className='timeVideo'>20:45</span></div>
+
+        <div className='hide'><span className='volumeAndSubtitle'>
+        <IconButton onClick={handleVolume}>
+        {volume ? <VolumeUpOutlinedIcon sx={{color: 'white'}}/> :
+         <VolumeOffOutlinedIcon sx={{color: 'white'}}/>}
+        </IconButton>
+         | 
+         <IconButton onClick={handleSubtitle}>
+         {subtitle ? <ClosedCaptionOutlinedIcon sx={{color: 'white'} }/> :
+         <ClosedCaptionDisabledOutlinedIcon sx={{color: 'white'} }/>}
+         </IconButton>
+            </span></div>
+
+    <div className='headerAndAvatatr'>
+        <div className='avatar'>
+            <IconButton>
+                <Avatar src='https://newprofilepic2.photo-cdn.net//assets/images/article/profile.jpg' />
+            </IconButton>
+        </div>
+        <div className='header'>
+            <p className='headerText'>test your skills in Javascript in our toturial</p>
+        </div>
+        <div className='moreOption'>
+            
+            <ModalVideo/>
+            
+        </div>
+    </div>  
+    <div className='channelNameDiv'>
+            <p className='channelName'>Javascript</p>
+    </div>
+    <div className='whatching'>
+            <p className='channelName'><span>2 days ago</span> <span className='dot'>•</span>views 3,847 </p>
+    </div>
+  </div>
+
 
   </section>
   </>
