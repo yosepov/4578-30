b3230b49a1917caeaa56be9968d10bdf39855666
ellisbena <alisbenarie@gmail.com>
2023-04-04 17:23:11 +0300

    Adding Icons and Data

--- src/Components/SideMenu/SideMenu.tsx
+++ src/Components/SideMenu/SideMenu.tsx
@@ -11,8 +11,17 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import HistoryIcon from '@mui/icons-material/History';
+import HomeIcon from '@mui/icons-material/Home';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
+
+
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import { alignProperty } from '@mui/material/styles/cssUtils';
+
 const drawerWidth = 240;
 
 const openedMixin = (theme: Theme): CSSObject => ({
@@ -105,20 +114,24 @@ export default function MiniDrawer(): JSX.Element {
         <MenuIcon />
       </IconButton>
     </Box>
+    
     <Box>
       <Drawer sx={{
             width:20,
             zIndex:-1,
-            display:'block',
+            display:'flex',
+            justifyContent:'center',
             position:`sticky`}} variant='permanent' open={open} >
        
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-              <ListItem key={text} disablePadding sx={{ display: 'flex' ,}}>
+          {['Home','Home'].map((text, index) => (
+              <ListItem key={text} disablePadding sx={{ display: 'flex'}}>
               <ListItemButton
                 sx={{
                     minHeight: 48,
                     justifyContent: open ? 'initial' : 'center',
+                    
+                    
                     px: 2.5,
                 }}
                 >
@@ -126,10 +139,12 @@ export default function MiniDrawer(): JSX.Element {
                   sx={{
                       minWidth: 0,
                       mr: open ? 3 : 'auto',
+                      display:'flex',
                       justifyContent: 'center',
                     }}
                     >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  <HomeIcon />
+                  <p style={{display:open? 'none' : 'block'}}>Home</p>
                 </ListItemIcon>
                 <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
               </ListItemButton>
@@ -137,8 +152,20 @@ export default function MiniDrawer(): JSX.Element {
           ))}
         </List>
         <Divider />
+
+
+
+
+
+
+
+
+
+
+
+
         <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          {['History', 'My Likes'].map((text, index) => (
               <ListItem key={text} disablePadding sx={{ display: 'block' }}>
               <ListItemButton
                 sx={{
@@ -154,13 +181,68 @@ export default function MiniDrawer(): JSX.Element {
                       justifyContent: 'center',
                     }}
                     >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  {index % 2 === 0 ? <HistoryIcon /> : <ThumbUpOffAltIcon />}
                 </ListItemIcon>
                 <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
               </ListItemButton>
             </ListItem>
           ))}
         </List>
+        <Divider />
+        <List>
+          {['Video Library', 'Subscriptions'].map((text, index) => (
+              <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                    minHeight: 48,
+                    justifyContent: open ? 'initial' : 'center',
+                    px: 2.5,
+                }}
+                >
+                <ListItemIcon
+                  sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : 'auto',
+                      justifyContent: 'center',
+                    }}
+                    >
+                  {index % 2 === 0 ? <VideoLibraryIcon /> : <SubscriptionsIcon />}
+                  
+                </ListItemIcon>
+                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+
+        <Divider />
+        <List>
+          {['Watch Later', 'My Videos'].map((text, index) => (
+              <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                    minHeight: 48,
+                    justifyContent: open ? 'initial' : 'center',
+                    px: 2.5,
+                }}
+                >
+                <ListItemIcon
+                  sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : 'auto',
+                      justifyContent: 'center',
+                    }}
+                    >
+                  {index % 2 === 0 ? <AccessTimeIcon sx={{display: open? 'initial': 'none'}}/> : <PlaylistPlayIcon sx={{display: open? 'initial': 'none'}} />}
+                </ListItemIcon>
+                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+
+
+        
       </Drawer>      
     </Box>
 </>
