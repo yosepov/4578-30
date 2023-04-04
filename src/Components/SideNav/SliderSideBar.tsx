c3202cf966184efaa00a8791a334e3a496eabc67
dov-z <dov.zilberman@gmail.com>
2023-04-04 19:23:57 +0300

    clean drawer

--- src/Components/SideNav/SliderSideBar.tsx
+++ src/Components/SideNav/SliderSideBar.tsx
@@ -5,46 +5,21 @@ import SideNav from './SideNav';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 
 export default function SliderSideBar() {
-    const [state, setState] = React.useState({
-        top: false,
-        left: false,
-        bottom: false,
-        right: false,
-    });
-
-    const toggleDrawer =
-        (anchor: 'left', open: boolean) =>
-            (event: React.KeyboardEvent | React.MouseEvent) => {
-                if (
-                    event &&
-                    event.type === 'keydown' &&
-                    ((event as React.KeyboardEvent).key === 'Tab' ||
-                        (event as React.KeyboardEvent).key === 'Shift')
-                ) {
-                    return;
-                }
-
-                setState({ ...state, [anchor]: open });
-            };
-
-
+    const [isOpen, setIsOpen] = React.useState(false);
+    const toggleDrawer = (isOpen: boolean) => setIsOpen(isOpen);
 
     return (
         <div >
-            {(['left'] as const).map((anchor) => (
-                <React.Fragment key={anchor} >
-                    <Button onClick={toggleDrawer(anchor, true)}><MenuOutlinedIcon color='action' /></Button>
-                    <SwipeableDrawer
-                        anchor={anchor}
-                        open={state[anchor]}
-                        onClose={toggleDrawer(anchor, false)}
-                        onOpen={toggleDrawer(anchor, true)}
-                    >
-                        <SideNav />
-
-                    </SwipeableDrawer>
-                </React.Fragment>
-            ))}
+            <React.Fragment >
+                <Button onClick={() => toggleDrawer(!isOpen)}><MenuOutlinedIcon color='action' /></Button>
+                <SwipeableDrawer
+                    anchor={'left'}
+                    open={isOpen}
+                    onClose={() => toggleDrawer(false)}
+                    onOpen={() => toggleDrawer(true)}>
+                    <SideNav />
+                </SwipeableDrawer>
+            </React.Fragment>
         </div>
     );
 }
