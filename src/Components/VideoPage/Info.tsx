a68dc1b2df9545366d2b79d6e645f19efcb14d5f
David <dydyd289@gmail.com>
2023-04-18 17:53:34 +0300

    update

--- src/Components/VideoPage/Info.tsx
+++ src/Components/VideoPage/Info.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const Info = () => {
 
   return <>
-    <Box>
+    <Box sx={{width: '640px'}}>
         <Typography>
         Testing is one of those things that people either love or hate. Usually testing is something that is hated, until you work on a project with good tests and you realize how amazing they are. In this video I am going to show you how to get started with testing in JavaScript using Jest. I will talk about the code you need in order to write tests, as well as show you some pitfalls of testing. At the end of the video I will breakdown the importance of testing and some best practices you can adhere to in order to make your tests amazing. 
         </Typography>
